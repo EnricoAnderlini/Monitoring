d18ad27f92e9c0a9cbee852c3f96c78a4b1ba9c7
EnricoAnderlini <114755811+EnricoAnderlini@users.noreply.github.com>
2022-11-04 17:06:27 +0100

    Update R_code_multivar.r

--- R_code_multivar.r
+++ R_code_multivar.r
@@ -8,7 +8,7 @@ setwd("~/lab/")
 
 # to upload our project use the following function:
 load("biomes_multivar.RData")
-ls()
+ls() # to read all the arguments or data
 
 head(biomes) # plot per species matrix
 
@@ -20,4 +20,20 @@ multivar # information about the analysis done
 
 plot(multivar)
 attach(biomes_types)
-ordiellipse(multivar, type, col=c("black", "red", "green", "blue"), kind = "ehull", lwd=3)
+ordiellipse(multivar, type, col=c("black", "red", "green", "blue"), kind = "ehull", lwd = 3) # plotting the points that belong to the same biome
+ordispider(multivar, type, col=c("black", "red", "green", "blue"), label = T) # same as ordiellipse but we eant to see a label
+
+# let's save the graph out of R
+pdf("myfirstoutput.pdf") 
+plot(multivar)
+ordiellipse(multivar, type, col=c("black", "red", "green", "blue"), kind = "ehull", lwd = 3) 
+ordispider(multivar, type, col=c("black", "red", "green", "blue"), label = T)
+dev.off()
+
+#Exercise: export a pdf
+# with only the multivar plot
+# 5 minutes
+pdf("multivarperse.pdf")
+plot(multivar)
+dev.off()
+
