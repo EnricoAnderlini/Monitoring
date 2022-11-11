a513f29568efb9a5ce02498267b2343c200b1fd5
EnricoAnderlini <114755811+EnricoAnderlini@users.noreply.github.com>
2022-11-11 17:51:17 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -2,11 +2,38 @@ install.packages("raster")
 
 library(raster)
 
-setwd("~/lab")
+setwd("C:/Users/enric/Desktop/lab")
+
 
 p224r63_2011 <- brick("p224r63_2011_masked.grd")   # name of the file we want to use with the brick function to create layers
 
 plot(p224r63_2011)
 # dev.off()
 
-cl <- colorRampPalette(c('black', 'grey', 'light gray'))
+cl <- colorRampPalette(c('black', 'grey', 'light grey'))(100) #100 for the gradation of colors 
+plot(p224r63_2011, col=cl)
+
+#plotting one band
+#solution 1
+plot(p224t63_2011$B1_sre, col=cl) #putting the name
+
+plot(p224r63_2011[[1]], col=cl)
+
+#Exercise: change the colour ramp palette with colours from dark to light blue 
+clb <- colorRampPalette(c('dark blue', 'blue', 'light blue'))(100)
+plot(p224r63_2011[[1]], col=clb)
+
+par(mfrow=c(1,2)) # 1 and 2 are coloumn and rows, we are creating new entities
+plot(p224r63_2011[[1]], col=cl)
+plot(p224r63_2011[[2]], col=cl)
+
+#Exercise: multiframe with 4 images(2x2)
+par(mfrow=c(2,2))
+plot(p224r63_2011[[1]], col=cl)
+plot(p224r63_2011[[2]], col=cl)
+plot(p224r63_2011[[3]], col=cl)
+plot(p224r63_2011[[4]], col=cl)
+
+
+
+
